dbf94fb7fb722c594cd6864995a9372afa2f3c41
Concept'IQ <ConceptIQ@users.noreply.github.com>
2019-01-10 12:36:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr-FR" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="language" content="fr-FR">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <base href="https://www.example.com/">
+        <meta name="application-name" content="JyMel">
+        <meta name="creator" content="Concept'IQ">
+        <meta name="author" content="/humans.txt">
+        <meta name="publisher" content="/humans.txt">
+        <meta name="generator" content="Atom Editor by GitHub">
+
+        <meta name="theme-c" content="#CCCCCC">
+
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <title>Document</title>
+
+        <link type="text/plain" rel="author" href="/humans.txt">
+        <link rel="stylesheet" href="/settings/css/master.css" media="screen">
+        <link rel="icon" href="favicon.ico">
+    </head>
+    <body>
+        <noscript>
+            <div class="container">
+
+            </div>
+        </noscript>
+
+        <footer>
+            <div class="container">
+
+            </div>
+        </footer>
+    </body>
+</html>
